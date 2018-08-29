7daceab4933790c0155269792f2a4ed2e775b551
Unknown <m.b.wolfsohn@gmail.com>
2018-08-28 19:42:27 -0500

    Square

--- tictactoe/src/tictactoe/Square.java
+++ tictactoe/src/tictactoe/Square.java
@@ -1,5 +1,28 @@
 package tictactoe;
 
-public class Square {
+enum Status {
+	x,
+	o,
+	q; // Question MArk
+}
 
+
+public class Square {
+	
+	private Status status;
+	
+	
+	public Square() {
+		
+	}
+	
+	public Status GetStatus() {
+		return status;
+	}
+	
+	public Status ChangeStatus(Status newStatus) {
+		status = newStatus;
+		return status;
+	}
+	
 }
