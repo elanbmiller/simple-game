cd28f24650aad5b333317f70690e46573866dd3c
Unknown <m.b.wolfsohn@gmail.com>
2018-08-28 19:37:47 -0500

    S

--- tictactoe/src/tictactoe/Board.java
+++ tictactoe/src/tictactoe/Board.java
@@ -1,5 +1,29 @@
 package tictactoe;
+import java.util.ArrayList;
+//import java.awt;
+
 
 public class Board {
+	
+	
+	ArrayList<ArrayList<Square>> squaresList = new ArrayList<ArrayList<Square>>();  // Row x, column y
 
+	
+	
+	public Board(int size) {
+		
+		// Create a squareList and initiate it
+		for(int i = 0; i < size; i++)  {
+	        squaresList.add(new ArrayList<Square>(size));
+		}
+		
+		
+		
+		
+		
+	}
+	
+	
+
+	
 }
