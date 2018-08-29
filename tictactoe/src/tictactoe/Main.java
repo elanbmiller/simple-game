eef0b8391a96d5b2af179cbe99d6871cb5015d82
Elan <elanbmiller@gmail.com>
2018-08-28 20:25:40 -0500

    creating initial board w/ question marks

--- tictactoe/src/tictactoe/Main.java
+++ tictactoe/src/tictactoe/Main.java
@@ -1,10 +1,48 @@
 package tictactoe;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
-public class Main {
+import javax.swing.*;
+
+public class Main extends JFrame{
+
+	public Main() {
+		super("Main");
+		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		super.add(new JLabel("Tic Tac Toe"));
+		//TODO: Create board w/ board_size
+		int dimension = this.get_dim();
+		JFrame frame = new JFrame("Flow Layout");
+		frame.setLayout(new GridLayout(dimension,dimension));
+		frame.setSize(500,500);  
+		frame.setVisible(true);  
+		
+		//TODO: Check if there's an unifinished game, if so, load it
+		//otherwise populate initial frame w/ question marks
+		
+		this.populate_init_frame(dimension, frame);
+		
+		super.pack();
+		super.setVisible(true);
+	}
+
+	public void populate_init_frame(int dimension, JFrame frame) {
+		//Create buttons
+		for (int i = 0; i < dimension*dimension; i++) {
+			JButton button = new JButton("?");
+			frame.add(button);
+		}
+	}
+
+	public int get_dim() {
+		int value = Integer.parseInt(JOptionPane.showInputDialog("Input Dimension"));
+		return value;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		new Main();
 	}
 
 }
