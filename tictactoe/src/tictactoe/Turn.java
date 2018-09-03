a600dd0467686985e395d5a3af096153ac265548
Noah Saffer <noah.saffer@wustl.edu>
2018-09-03 15:24:43 -0500

    Javadoc for Turn.java

--- tictactoe/src/tictactoe/Turn.java
+++ tictactoe/src/tictactoe/Turn.java
@@ -1,5 +1,13 @@
 package tictactoe;
 
+/**
+ * @boolean isXsTurn is used to keep track of which player's turn it is
+ *
+ * @method changeTurn() is used to NOT the current player turn to change it
+ *
+ * @method isItXsTurn() is the getter for isXsTurn
+ */
+
 public class Turn {
 	boolean isXsTurn;
 	
